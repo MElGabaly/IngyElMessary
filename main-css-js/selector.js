11f48422f679105be7f5489d1950c6a65e763efe
Mo <Gabalyma@gmail.com>
2020-06-04 01:16:37 -0700

    Fixed the selection option

--- main-css-js/selector.js
+++ main-css-js/selector.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
   var $container = $(".portfolioContainer");
   $container.isotope({
+    percentPosition: true,
     filter: "*",
     animationOptions: {
       duration: 750,
       easing: "linear",
       queue: false
-    }
+    },
+    initLayout: true
   });
 
   $(".portfolioFilter a").click(function() {
@@ -15,12 +17,14 @@ $(document).ready(function() {
 
     var selector = $(this).attr("data-filter");
     $container.isotope({
+      percentPosition: true,
       filter: selector,
       animationOptions: {
         duration: 750,
         easing: "linear",
         queue: false
-      }
+      },
+      initLayout: true
     });
     return false;
   });
