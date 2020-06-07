e1f2c17e59a09d6a17351366f4abf1f7e9715496
Mo <Gabalyma@gmail.com>
2020-06-07 02:06:30 -0700

    trial

--- contact/handler.js
+++ contact/handler.js
@@ -33,7 +33,7 @@ module.exports.staticSiteMailer = (event, context, callback) => {
       statusCode: err ? 500 : 200,
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "http://ingyelmessary.com"
       },
       body: JSON.stringify({
         message: err ? err.message : data
